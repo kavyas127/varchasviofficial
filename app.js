eae989e4e7337ff829efa85e90bf06c4ae7eb46b
kavyas127 <officialkesai315@gmail.com>
2024-06-25 19:09:41 +0530

    Node init

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/items", function (req, res) {
+    res.render("items");
+});
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000");
+});
